8dcc6bdd051efe9fd3cba325ead182606702916c
Andre Proenca <andreproenca@hotmail.com>
2024-11-19 17:39:27 -0400

    tests ok

--- src/main/java/com/devsuperior/desafioum/app/DesafioumApplication.java
+++ src/main/java/com/devsuperior/desafioum/app/DesafioumApplication.java
@@ -22,7 +22,20 @@ public class DesafioumApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Order order = new Order(1034, 150.00, 20.0);
+		Order order2 = new Order(2282, 800.00, 10.0);
+		Order order3 = new Order(1309, 95.90, 0.0);
+		System.out.println("------RESULTADO--------");
 		System.out.println("Pedido código: " + order.getCode());
-		System.out.println("Valor total: " + orderService.calculateTotal(order));
+		double total = orderService.calculateTotal(order);
+		System.out.println("Valor total: R$ " + String.format("%.2f", total));
+		System.out.println("----------------");
+		System.out.println("Pedido código: " + order2.getCode());
+		double total2 = orderService.calculateTotal(order2);
+		System.out.println("Valor total: R$ " + String.format("%.2f", total2));
+		System.out.println("----------------");
+		System.out.println("Pedido código: " + order3.getCode());
+		double total3 = orderService.calculateTotal(order3);
+		System.out.println("Valor total: R$ " + String.format("%.2f", total3));
+
 	}
 }
